cf27d95027237b6b570e5b30a4b513ad0711936a
Wilmerson da Silva <wilmersondasilva@gmail.com>
2019-10-24 01:56:34 -0300

    Fixs Login bug related to responsiveness

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -71,7 +71,6 @@ class Login extends Component {
             <Container fluid>
                 <Row className="pt-5">
                     <Col
-                        xs={{ size: 10, offset: 1 }}
                         sm={{ size: 8, offset: 2 }}
                         md={{ size: 6, offset: 3 }}
                         lg={{ size: 4, offset: 4 }}
